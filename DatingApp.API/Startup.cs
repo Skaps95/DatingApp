85c9ce8121d4e86ce70737856f36a717cd688e98
Sahaj <sahajkapoor95@gmail.com>
2020-03-24 02:05:45 +0530

    Added AuthRepo and partial UserController

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors(); //So that angular app can communicate
+            services.AddScoped<IAuthRepository,AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
