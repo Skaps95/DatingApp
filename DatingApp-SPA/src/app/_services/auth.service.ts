800e0e036d05b42a307bae72bc938621856bc256
Sahaj Kapoor <sahajkapoor95@gmail.com>
2020-04-06 01:04:23 +0530

    Added login and registration in Angular

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,10 @@ login(model: any) {
       localStorage.setItem('token', user.token);
     }
   }));
+}
+
+register(model: any) {
+  return this.http.post(this.baseurl + 'register', model);
 }
 
 
